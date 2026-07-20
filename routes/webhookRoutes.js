a814a5fee11f76b191b06e135b7abfbeaf96efd1
Tantichariyaphan <goter5555@gmail.com>
2026-07-20 18:52:29 +0700

    Update webhookRoutes.js

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -5,20 +5,19 @@ const { webhookLimiter } = require('../middleware/rateLimiter');
 const asyncHandler = require('../utils/asyncHandler');
 
 const router = express.Router();
-router.post('/', webhookLimiter, createLineWebhookMiddleware(), asyncHandler(async (req, res) => {
-  await processWebhookEvents(req.body.events || []);
-  res.status(200).end();
-}));
+
 router.post(
-'/',
-webhookLimiter,
-createLineWebhookMiddleware(),
-asyncHandler(async(req,res)=>{
-console.log("===== WEBHOOK =====");
-console.log(req.body);
-await processWebhookEvents(req.body.events||[]);
-res.status(200).end();
-})
+  '/',
+  webhookLimiter,
+  createLineWebhookMiddleware(),
+  asyncHandler(async (req, res) => {
+    console.log('===== WEBHOOK ROUTE HIT =====');
+    console.log(JSON.stringify(req.body, null, 2));
+
+    await processWebhookEvents(req.body.events || []);
+
+    res.status(200).end();
+  })
 );
 
 module.exports = router;
